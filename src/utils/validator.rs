50007b4bf6015fde118ebd1fb7ac7081c78eae0d
Mohammed M <mohamgoham@gmail.com>
2023-04-05 02:48:53 -0400

    add validator

--- src/utils/validator.rs
+++ src/utils/validator.rs
@@ -13,3 +13,65 @@ pub fn validate_interface(v: String) -> Result<> {
         },
     }
 }
+
+pub fn validate_host_opt(v: String) -> Result<(), String> {
+    let addr = IpAddr::from_str(&v);
+    match addr {
+        Ok(_) => {
+            return Ok(())
+        },
+        Err(_) => {
+            return Err(String::from("Please specify Ip Addr"));
+        }
+    }
+}
+
+pub fn validate_port_opt(v: String) -> Result<(), String> {
+    match v.parse::<u16>() {
+        Ok(_) => {
+            return Ok(())
+        },
+        Err(_) => {
+            return Err(String::from("Please Specify port number!!"));
+        }
+    }
+}
+
+pub fn validate_duration_opt(v: String) -> Result<(), String> {
+    match v.parse::<u64> {
+        Ok(_) => {
+            return Ok(())
+        },
+        Err(_) => {
+            return Err(String::from("Please specift port number"));
+        }
+    }
+}
+
+pub fn validate_protocol(v: String) -> Result<(), String> {
+    let validate_protocol = db::get_protocol_list();
+    let protocol_vec: Vec<&str> = v.trim().split(",").collect();
+    for protocol in protocol_vec {
+        if !valid_protocols.contains(&protocol.to_string()) {
+            return Err(String::from("Invalid protocol"));
+        }
+    }
+    Ok(())
+}
+
+pub fn validate_host_port(v: String) -> Result<(), String> {
+    let host_valid = match validate_host_opt(v.clone()) {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+    let port_valid = match validate_port_opt(v) {
+        Ok(_) => true, 
+        Err(_) => false,
+    };
+    if host_valid || port_valid {
+        Ok(())
+    } else {
+        err(String::from("Please specify Ip Address or port Number!!"))
+    }
+}
+
